070ad71f9ae7c58d552c39d8960bc74fe43b5c98
RB2003-BB <6531501105@lamduan.mfu.ac.th>
2025-04-20 23:38:57 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -15,10 +15,9 @@ from sklearn.decomposition import PCA
 st.set_page_config(page_title="K-Means Clustering", layout="wide")
 
 # App title
-st.title("K-Means App with Iris Dataset by Rapeepan Srisuwan 6531501105")
+st.title("🔍 K-Means Clustering Visualizer by Rapeepan Srisuwan naja eiei")
 
 # Sidebar slider to select k
-print("Configure Clustering")
 k = st.sidebar.slider("Select number of clusters (k)", min_value=2, max_value=10, value=4)
 
 # Load Iris dataset
